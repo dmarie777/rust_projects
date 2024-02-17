00613eef9b2cf944d625a62d1549e67dbd5ed135
dmarie777 <deimarie777@gmail.com>
2024-02-17 12:26:57 -0500

    Added new constant

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,8 @@
+const MY_CONSTANT: u32 = 3 * 3;
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 7;
     println!("The value of x is: {x}");
+    println!("{MY_CONSTANT}")
 }
